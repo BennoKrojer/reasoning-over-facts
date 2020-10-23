4c35200a7eda29cf585679b9460fc5618fe3204b
BennoKrojer <benno.krojer@gmail.com>
2020-10-23 13:19:26 +0200

    clean up composition data

--- data/compositional/datasets/StandardComp_with_anti/datagen_config.py
+++
@@ -1,8 +0,0 @@
-shuffle_train = False
-train_eval_random_subset = True
-RELATIONTYPE_AMOUNT = 100
-ENTITYTYPE_AMOUNT = 5000
-FACTS_PER_RELATION = 800
-MAX_INSTANCES_PER_FACT = 1
-ratio_of_pattern_relations = 0.5  # percentage of relations that show the specific pattern
-ratio_of_complete_patterns = 0.9  # for the relations that show pattern, how many patterns are complete in training
